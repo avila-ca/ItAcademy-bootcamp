821613cf0b1f0966a5245e568d6a1aef3fb8f607
avila-ca <avilacalaf@gmail.com>
2023-08-30 12:42:58 +0200

    3 throttle tests ready

---
+++ sprint2/throttle/src/tests/throttle.test.ts
@@ -0,0 +1,43 @@
+import { afterEach } from 'node:test';
+import throttle from '../throttle';
+
+
+describe('Throttle function', () => {
+
+    
+    test('calling 3 times to throttle but nothing executes the function inmediatly',() => {
+        const mockFunction = jest.fn();
+        const throttledFn = throttle(mockFunction, 'somevalue', 200);
+
+        throttledFn('value1');
+        throttledFn('value2');
+        throttledFn('value3');
+
+        expect(mockFunction).not.toHaveBeenCalled();
+    })
+    test('calling 3 times to throttle but only executes once', () =>{
+        jest.useFakeTimers();
+
+        const mockFunction = jest.fn();
+        const throttledFn = throttle(mockFunction, 'somevalue', 200);
+        
+        throttledFn('value1');
+        throttledFn('value2');
+        throttledFn('value3');
+
+        jest.advanceTimersByTime(200);
+        expect(mockFunction).toHaveBeenCalledTimes(1);
+    })
+    test('calling 3 times to throttle but only executes the last value', () =>{
+        jest.useFakeTimers();
+
+        const mockFunction = jest.fn();
+        const throttledFn = throttle(mockFunction, 'somevalue', 200);
+        
+        throttledFn('value1');
+        throttledFn('value2');
+        throttledFn('value3');
+        jest.advanceTimersByTime(200);
+        expect(mockFunction).toHaveBeenCalledWith('value3');
+    })
+})
