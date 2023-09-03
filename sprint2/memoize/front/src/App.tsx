32ebb4099ee550bf4a976bb96167c625b7a3d523
avila-ca <avilacalaf@gmail.com>
2023-09-03 10:21:55 +0200

    Improving tests and frontend ready

--- sprint2/memoize/front/src/App.tsx
+++ sprint2/memoize/front/src/App.tsx
@@ -1,33 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react';
+import {memoize} from '../../cli/memoize';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [num, setNum] = useState('');
+  const [output, setOutput] = useState(0);
 
+  const fn = (num:number) => {
+    let totalLoops:number = 0;
+
+    for (let index = 0; index < num; index++) {
+      for (let indexb = 0; indexb < num; indexb++) {
+        for (let indexc = 0; indexc < num; indexc++) {
+          totalLoops++;
+        }
+      }
+    }
+    return totalLoops;
+  }
+
+  const memoized = memoize(fn); 
+  let result:number = 0;
+  function handleOnclickButton(){
+    
+    const n = Number(num)
+    setOutput(memoized(n));    
+  }
+
+  const handleInputChange = (event:React.ChangeEvent<HTMLInputElement>) =>{
+    const newInput:string = event.target.value;
+    
+    setNum(newInput);
+  }
+  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input type="number" onChange={handleInputChange} autoFocus placeholder='put a  number'/>
+      <button onClick={handleOnclickButton}>Press here to see the result</button>
+      <input type="text" value={output} readOnly/>
     </>
   )
 }
--- sprint2/memoize/tests/memoize.test.ts
+++ sprint2/memoize/tests/memoize.test.ts
@@ -1,20 +1,31 @@
 import {memoize} from '../cli/memoize';
-
+import { loopFn } from '../cli/cli';
 
 describe('memoize function', () => {
 
     
-    test('calling to memoize and executes the function inmediatly',() => {
+    test('calling to memoize and trying to execute correctly 2 times',() => {
         
-        const logMock = jest.fn(); 
+        const mockFunc = jest.fn(); 
     
-        console.log = logMock; 
+        console.log = mockFunc; 
     
-        const memoizedFunction = memoize();
+        const memoizedFunction = memoize(mockFunc);
     
-        memoizedFunction(); 
-        memoizedFunction(); 
+        memoizedFunction(12); 
+        memoizedFunction(12); 
     
-        expect(logMock).toHaveBeenCalledTimes(2);
+        expect(mockFunc).toHaveBeenCalledTimes(2);
+    })
+    test('Calling the function with 2 and would return 4',() => {
+
+        const mockFn = jest.fn(); 
+    
+    
+        const memoizedFunction = memoize(loopFn);
+    
+        //memoizedFunction(2);  
+    
+        expect(memoizedFunction(2)).toBe(4);
     })
 })
